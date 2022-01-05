23bae0fafd6eb92e47c06951654588263b23d011
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-05 11:51:02 +0530

    simple integer multiplication;

---
+++ src/pro4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class pro4 {
+    public static void main(String[] args) {
+        int n1 ,n2;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter first number");
+        n1 = sc.nextInt();
+        System.out.println("enter second number");
+        n2 = sc.nextInt();
+        System.out.println("multiplication of given numbers is:- "+n1*n2);
+
+    }
+}
