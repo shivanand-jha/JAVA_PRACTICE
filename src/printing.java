969ae1b8ea07f3a2875c90acf0591139f87879ae
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-11 23:31:21 +0530

    printing

---
+++ src/printing.java
@@ -0,0 +1,19 @@
+public class printing {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 45.6f;
+        char c = 'A';
+//        System.out.println(a+""+b+""+c);
+//        System.out.println(a+" "+b+" "+c);
+//        System.out.println(a+b+c);
+//        System.out.println(a+b+" "+c);
+        System.out.printf("hello %d%f%c\n",a,b,c);//printing without spaces simple concatenation of string
+        System.out.printf("hello %d %f %c\n",a,b,c);//printing with spaces
+        System.out.printf("hello %o \n",a);//printing octal value
+        System.out.printf("hello %x \n",a);//printing hexadecimal value
+        System.out.printf("hello  %e \n",b);//floating point representation in +ve direction.
+        System.out.printf(" hello %g \n",b);//floating point representation in -ve direction.
+
+
+    }
+}
