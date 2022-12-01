5a3d956b6825039b2b12c086a67753ff592f152b
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-12-01 18:38:06 +0530

    Initial commit

--- src/ascii_Fullwidth_Printing.java
+++ src/ascii_Fullwidth_Printing.java
@@ -1,6 +1,6 @@
 public class ascii_Fullwidth_Printing {
     public static void main(String[] args) {
-        int as = 0Xff01;//hexadecimal value for first ascii fullwidth
+        int as = 0Xff01;//hexadecimal value for first ascii full width
         for(int i=as; i<=0Xff64;i++)
         {
             System.out.print((char)i);
