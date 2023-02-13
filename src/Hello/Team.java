38fdf6f1635311d01d0339eab2f6ea89e21eb82b
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2023-02-13 10:02:39 +0530

    Java Practice

---
+++ src/Hello/Team.java
@@ -0,0 +1,47 @@
+package Java_P.Hello;
+
+class Hello {
+public Hello(){
+    System.out.println("Hello world !!!");
+}
+}
+ public class  Team {
+    Member member ;
+
+    public Team(Member myMember) {
+        this.member = myMember;
+    }
+
+    public static void main(String[] args) {
+         Member myMember = new Member("Aurieel", "light", 10, 1);
+         Team myTeam = new Team(myMember);
+         System.out.println(myTeam.member.getName());
+         System.out.println(myTeam.member.getType());
+         System.out.println(myTeam.member.getLevel());
+         System.out.println(myTeam.member.getRank());
+     }
+ }
+class Member {
+    private String name;
+    private String type;
+    private int level;
+    private int rank;
+    public Member(String name, String type, int level, int rank) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.rank = rank;
+    }
+    public String getName() { // what is your name?
+        return this.name; // my name is ...
+    }
+    public String getType() { // what is your type?
+        return this.type; // my type is ...
+    }
+    public int getLevel() { // what is your level?
+        return this.level; // my level is ...
+    }
+    public int getRank() { // what is your rank?
+        return this.rank; // my rank is
+    }
+}
---
+++ src/Type_Conversion/Numeric_Primititve.java
@@ -0,0 +1,66 @@
+package Java_P.Type_Conversion;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Numeric_Primititve {
+    public static void main(String[] args) {
+//        byte byt = 42;
+//        short shrt = byt;
+//        int intVar = byt;
+//        long longVar = intVar;
+//        float floatVar = longVar;
+//        double doVar = floatVar;
+//        double doubleVar = 42.0d;
+//        float floatVar = (float) doubleVar;
+//        long longVar = (long) floatVar;
+//        int intVar = (int) longVar;
+//        short shortVar = (short) intVar;
+//        byte byteVar = (byte) shortVar;
+//        System.out.println(byt);
+//        System.out.println(shortVar);
+//        System.out.println(intVar);
+//        System.out.println(longVar);
+//        System.out.println(floatVar);
+//        System.out.println(doubleVar);
+//        char char1 = 1, char2 = 2;
+//        short short1 = 1, short2 = 2;
+//        int int1 = 1, int2 = 2;
+//        float float1 = 1.0f, float2 = 2.0f;
+//        int1 = char1 + char2;
+//        System.out.println(int1);
+//        int1 = short1 + short2;
+//        System.out.println(int1);
+//        int1 = char1 + short2;
+//        System.out.println(int1);
+//        float1 = short1 + float2;
+//        System.out.println(float1);
+//        int1 = int1 + int2;
+//        System.out.println(int1);
+//        char char1
+//                = (char)
+//                65; // A
+//        System.out.println(char1);
+//        byte byte1
+//                = (byte) 'A'; // 65
+//        System.out.println(byte1);
+//        short short1 = (short) 'A'; // 65
+//        System.out.println(short1);
+//        int int1
+//                = (int)
+//                'A'; // 65
+//        System.out.println(int1);
+//        char char2= (char) 8253; // ‽
+//        System.out.println(char2);
+//        byte byte2= (byte)
+//                '‽'; // 61 (truncated code-point into the ASCII range)
+//        System.out.println(byte2);
+//        short short2 = (short) '‽';
+//        System.out.println(short2);
+//        int int2
+//                = (int)
+//                '‽';
+//        System.out.println(int2);
+    }
+
+}
